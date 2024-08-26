553c69584e4414d0921d0f1376bac21b344c11a0
Jason Long <j@jaslong.com>
2024-08-26 01:13:34 -0700

    Add custom keys
    GUITAB for window tabbing
    ARROW "=>" in symbol layer

--- keyboards/keyboardio/atreus/keymaps/jaslong/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/jaslong/keymap.c
@@ -38,13 +38,40 @@
 (ch) == '?' ? KC_QUESTION : \
 KC_NO)
 
+enum custom_keycodes {
+    // For window tabbing, equivalent to KC_LGUI + MO(5).
+    GUITAB = SAFE_RANGE,
+    // Sends "=>"
+    ARROW,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case GUITAB:
+      if (record->event.pressed) {
+        register_code(KC_LGUI);
+        layer_on(5);
+      } else {
+        layer_off(5);
+        unregister_code(KC_LGUI);
+      }
+      break;
+    case ARROW:
+      if (record->event.pressed) {
+        SEND_STRING("=>");
+      }
+      break;
+  }
+  return true;
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Colemak
   [0] = LAYOUT(
-    LT(4,KC_Q)   , KC_W         , KC_F         , KC_P         , KC_B         ,                               KC_J         , KC_L         , KC_U         , KC_Y         , LT(4,K('\'')),
+    KC_Q         , KC_W         , KC_F         , KC_P         , KC_B         ,                               KC_J         , KC_L         , KC_U         , KC_Y         , LT(4,K('\'')),
     KC_A         , KC_R         , KC_S         , KC_T         , KC_G         ,                               KC_M         , KC_N         , KC_E         , KC_I         , KC_O         ,
     LT(3,KC_Z)   , KC_X         , KC_C         , KC_D         , KC_V         , KC_ESC       , KC_TAB       , KC_K         , KC_H         , K(',')       , K('.')       , LT(4,K('/')) ,
-    ____TRNS____ , ____TRNS____ , KC_LGUI      , KC_LSFT      , KC_LCTL      , KC_BSPC      , KC_ENT       , KC_SPC       , MO(2)        , KC_LALT      , ____TRNS____ , ____TRNS____
+    GUITAB       , ____TRNS____ , KC_LGUI      , KC_LSFT      , KC_LCTL      , KC_BSPC      , KC_ENT       , KC_SPC       , MO(2)        , KC_LALT      , ____TRNS____ , ____TRNS____
   ),
   // QWERTY
   [1] = LAYOUT(
@@ -55,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   // Symbol
   [2] = LAYOUT(
-    _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ ,                               _____NO_____ , K('[')       , K(']')       , _____NO_____ , K('`')       ,
+    _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ ,                               _____NO_____ , K('[')       , K(']')       , ARROW        , K('`')       ,
     K('~')       , K('_')       , K('-')       , K('=')       , K('+')       ,                               K('|')       , K('(')       , K(')')       , K(':')       , K(';')       ,
     K('!')       , K('@')       , K('#')       , K('$')       , K('%')       , K('^')       , K('&')       , K('*')       , K('{')       , K('}')       , _____NO_____ , K('\\')      ,
     ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , A(KC_BSPC)   , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____
@@ -69,10 +96,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   // OS
   [4] = LAYOUT(
-    _____NO_____ , KC_LGUI      , KC_TAB       , KC_LSFT      , K('`')       ,                               _____NO_____ , KC_MPRV      , KC_MPLY      , KC_MNXT      , _____NO_____ ,
+    _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ ,                               _____NO_____ , KC_MPRV      , KC_MPLY      , KC_MNXT      , _____NO_____ ,
     _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ ,                               _____NO_____ , G(K('$'))    , C(G(KC_SPC)) , KC_MUTE      , _____NO_____ ,
     _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ , ____TRNS____ , ____TRNS____ , _____NO_____ , G(K('%'))    , KC_BRMU      , KC_VOLU      , _____NO_____ ,
     QK_BOOT      , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , KC_BRMD      , KC_VOLD      , ____TRNS____
+  ),
+  // GUITAB
+  [5] = LAYOUT(
+    _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ ,                               _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ ,
+    _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ ,                               _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ ,
+    _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ , ____TRNS____ , ____TRNS____ , _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ , _____NO_____ ,
+    ____TRNS____ , K('`')       , KC_TAB       , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____ , ____TRNS____
   )
 };
 
